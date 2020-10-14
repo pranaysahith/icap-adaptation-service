9a3ae908acb0baf454a4129aa01b5c8cd0737cf0
Phil Taprogge <phil.taprogge@codurance.com>
2020-10-14 16:36:18 +0100

    Add some comments

--- cmd/adaptation-service.go
+++ cmd/adaptation-service.go
@@ -1,11 +1,13 @@
 package main
 
-import (
-	"log"
-	"encoding/json"
+// I'd recommend adding prometheus metrics to help with observability
 
-	"github.com/streadway/amqp"
+import (
+	"encoding/json"
+	"log"
+
 	pod "github.com/icap-adaptation-service/pkg"
+	"github.com/streadway/amqp"
 )
 
 var exchange = "adaptation-exchange"
@@ -13,6 +15,7 @@ var routingKey = "adaptation-request"
 var queueName = "adaptation-request-queue"
 
 func main() {
+	// hard-coded queue. should be env var
 	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq-service:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -21,10 +24,10 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	err = ch.ExchangeDeclare(exchange, "direct", true, false, false, false, nil,)
+	err = ch.ExchangeDeclare(exchange, "direct", true, false, false, false, nil)
 	failOnError(err, "Failed to declare an exchange")
 
-	q, err := ch.QueueDeclare(queueName, false, false, false, false,	nil)
+	q, err := ch.QueueDeclare(queueName, false, false, false, false, nil)
 	failOnError(err, "Failed to declare a queue")
 
 	err = ch.QueueBind(q.Name, routingKey, exchange, false, nil)
@@ -49,11 +52,33 @@ func main() {
 
 			podArgs, err := pod.NewPodArgs(fileID, input, output)
 			failOnError(err, "Failed to initialize Pod")
-			
+
 			err = podArgs.CreatePod()
+			// this is might be dangerous:
+			// I am not sure about Rabbit semantics, but I suspect at least the current
+			// mesasge (which is probably valid) might be lost if we fail to spawn a Pod
+			// We should retry the k8s operation. Maybe we should put the message back?
 			failOnError(err, "Failed to start Pod")
 		}
 	}()
+
+	// this is dangerous: terminating the program with CTRL-C will immediately kill all running go-routines
+	// potentially dropping messages on the floor.
+	// Better: Use a close channel to indicate to the running goroutine that it should exit after finishing the current
+	// message(s):
+	// * Use sync.WaitGroup to track goroutines
+	// * Instead of for d := range msgs, use select to read either from msgs or a stop channel
+	//   Example: https://github.com/gargath/pleiades/blob/7e810c6d91e7b524ee09f6ab7afabf89ac3fb418/pkg/aggregator/kafka/aggregator.go#L81-L101
+	// * Close the stop channel when CTRL-C is received and wait for the waitgroup before exiting
+	//   Example: https://github.com/gargath/pleiades/blob/7e810c6d91e7b524ee09f6ab7afabf89ac3fb418/pkg/aggregator/kafka/aggregator.go#L127-L130
+	//
+	// To capture CTRL-C you can use something like this:
+	//   quit := make(chan os.Signal, 1)
+	//   signal.Notify(quit, os.Interrupt)
+	//   go func() {
+	// 	   <-quit
+	// 	   aBlockingFunctionThatGracefullyShutsDownGoRoutines()
+	//   }()
 
 	log.Printf("[*] Waiting for messages. To exit press CTRL+C")
 	<-forever
--- pkg/pod.go
+++ pkg/pod.go
@@ -1,10 +1,10 @@
 package pod
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
-	"context" 
 
 	guuid "github.com/google/uuid"
 	core "k8s.io/api/core/v1"
@@ -15,16 +15,21 @@ import (
 )
 
 type PodArgs struct {
-	PodNamespace    string
-	Client          *kubernetes.Clientset
-	FileID          string
-	Input           string
-	Output          string
+	PodNamespace string
+	Client       *kubernetes.Clientset
+	FileID       string
+	Input        string
+	Output       string
 }
 
-func NewPodArgs(fileId, input, output string) (podArgs *PodArgs, err error){
+// more of a style thing: I consider named returns a bit of an anti-pattern
+// explicit returns make it clear when reading the function body what is being returned
+func NewPodArgs(fileId, input, output string) (podArgs *PodArgs, err error) {
 	podNamespace := os.Getenv("POD_NAMESPACE")
 
+	// the environment never changes after startup. It would be cheaper (and cleaner)
+	// to check for this once on start and store the value locally. `os.Getenv` is relatively expensive
+	// and we know what the value will be every time
 	if podNamespace == "" {
 		err = fmt.Errorf("init failed: POD_NAMESPACE environment variable not set")
 		return
@@ -42,10 +47,10 @@ func NewPodArgs(fileId, input, output string) (podArgs *PodArgs, err error){
 
 	podArgs = &PodArgs{
 		PodNamespace: podNamespace,
-		Client:	client,
-		FileID: fileId,
-		Input: input,
-		Output: output,
+		Client:       client,
+		FileID:       fileId,
+		Input:        input,
+		Output:       output,
 	}
 	return
 }
@@ -54,6 +59,8 @@ func (pa PodArgs) CreatePod() error {
 	podSpec := pa.GetPodObject()
 
 	pod, err := pa.Client.CoreV1().Pods(pa.PodNamespace).Create(context.TODO(), podSpec, metav1.CreateOptions{})
+	// There are a few reasons why this might fail, including rate limiting
+	// I'd suggest retrying this a few time with an exponential backoff
 	if err != nil {
 		return err
 	}
@@ -62,18 +69,23 @@ func (pa PodArgs) CreatePod() error {
 		log.Printf("Successfully created Pod")
 	}
 
+	// While unlikely, we should check for the case where pod == nil && err == nil
+	// Bugs in libraries do happen...
+
 	return nil
 }
 
+// I feel the Pod we spawn should have CPU and memory limits set to prevent a bug or malicious document from
+// crashing the node
 func (pa PodArgs) GetPodObject() *core.Pod {
 	return &core.Pod{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "rebuild-" + guuid.New().String(),
+			Name:      "rebuild-" + guuid.New().String(),
 			Namespace: pa.PodNamespace,
 		},
 		Spec: core.PodSpec{
 			ImagePullSecrets: []core.LocalObjectReference{{Name: "regcred"}},
-			RestartPolicy: core.RestartPolicyNever,
+			RestartPolicy:    core.RestartPolicyNever,
 			Volumes: []core.Volume{
 				{
 					Name: "sourcedir",
@@ -94,8 +106,8 @@ func (pa PodArgs) GetPodObject() *core.Pod {
 			},
 			Containers: []core.Container{
 				{
-					Name: "rebuild",
-					Image: "glasswallsolutions/icap-request-processing",
+					Name:            "rebuild",
+					Image:           "glasswallsolutions/icap-request-processing",
 					ImagePullPolicy: core.PullIfNotPresent,
 					Env: []core.EnvVar{
 						{Name: "FILE_ID", Value: pa.FileID},
@@ -103,8 +115,8 @@ func (pa PodArgs) GetPodObject() *core.Pod {
 						{Name: "OUTPUT_PATH", Value: pa.Output},
 					},
 					VolumeMounts: []core.VolumeMount{
-						{Name: "sourcedir",	MountPath: "/input"},
-						{Name: "targetdir",	MountPath: "/output"},
+						{Name: "sourcedir", MountPath: "/input"},
+						{Name: "targetdir", MountPath: "/output"},
 					},
 				},
 			},
